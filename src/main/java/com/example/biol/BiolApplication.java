f35e6c29d0d6e98f01573cfdf3468bd6500b2406
Железнякович Максим <maxssmail572@gmail.com>
2023-05-15 17:27:08 +0300

    Initial commit

---
+++ src/main/java/com/example/biol/BiolApplication.java
@@ -0,0 +1,13 @@
+package com.example.biol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/biol/BiolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
