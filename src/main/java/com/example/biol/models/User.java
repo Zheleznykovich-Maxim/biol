ec490e4976699c40faf8b2a45e1ea07246abef0f
Железнякович Максим <maxssmail572@gmail.com>
2023-05-21 19:43:58 +0300

    lesson-7

--- src/main/java/com/example/biol/models/User.java
+++ src/main/java/com/example/biol/models/User.java
@@ -41,6 +41,9 @@ public class User implements UserDetails {
     }
 
     //Security
+    public boolean isAdmin() {
+        return roles.contains(Role.ROLE_ADMIN);
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
